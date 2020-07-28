63fbe5a3b221f8538ef1f3521a6fa86056226594
INotFound <INotFound@github.com>
2020-07-28 22:37:32 +0800

    QML-Plugin

--- PluginTool/Launcher/Source/LauncherActivator.cpp
+++ PluginTool/Launcher/Source/LauncherActivator.cpp
@@ -1,11 +1,18 @@
+
 #include <PluginContext.h>
+#include <QQmlApplicationEngine>
+
 #include "LauncherService.h"
 #include "LauncherActivator.h"
+
 namespace Launcher{
     LauncherActivator::~LauncherActivator(){
     }
 
     void LauncherActivator::onStart(Safe<Magic::PluginContext>& ctx){
+        auto& engineRef = ctx->getProperty("QmlEngine");
+        auto* engine = std::any_cast<QQmlApplicationEngine*>(engineRef);
+        engine->load("qrc:/main.qml");
     }
 
     void LauncherActivator::onStop(Safe<Magic::PluginContext>& ctx){
--- main.cpp
+++ main.cpp
@@ -1,20 +1,43 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[])
-{
+#include <FileUtils.h>
+#include <PluginFrameWork.h>
+
+int main(int argc, char *argv[]){
+    QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+    Magic::Thread::SetName("Master");
+    std::list<std::string> pluginPaths;
+    Safe<Magic::ILogAppender> stdOutAppender(new Magic::StdOutLogAppender);
+    Safe<Magic::ILogAppender> fileOutAppender(new Magic::FileLogAppender("log.txt"));
+    Magic::LogAppenderMgr::GetInstance()->addILogAppender(fileOutAppender);
+    Magic::LogAppenderMgr::GetInstance()->addILogAppender(stdOutAppender);
+    Safe<QQmlApplicationEngine> engine(new QQmlApplicationEngine);
+    Safe<Magic::PluginFramework> frameWork(new Magic::PluginFramework);
+    std::string pluginToolPath = QGuiApplication::applicationDirPath().toStdString().append("/PluginTool");
+    frameWork->setProperty("QmlEngine",engine.get());
+    if(Magic::ReadFileList(pluginToolPath,pluginPaths) && !pluginPaths.empty()){
+        for(auto& v : pluginPaths){
+            if(v.find(".dll") != std::string::npos){
+                frameWork->installPlugin(v);
+            }
+        }
+        auto& plugins = frameWork->getPlugins();
+        
+        if(plugins.empty()){
+            MAGIC_FATAL() << "Not Found Plugins";
+            return EXIT_FAILURE;
+        }
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+        for(auto& v: plugins){
+            v.second->start();
+        }
 
+    }else{
+        return EXIT_FAILURE;
+    }
     return app.exec();
 }
